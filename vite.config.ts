4d3f1aeb20eda825d59497b5794c72ca9f5452dc
Luat <nluat6868@gmail.com>
2025-12-04 17:16:52 +0700

    yp

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': 'AIzaSyAEzGJ1L57xv-4YLuqQVIhwVoo8spiur58',
-        'process.env.GEMINI_API_KEY': 'AIzaSyAEzGJ1L57xv-4YLuqQVIhwVoo8spiur58'
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
